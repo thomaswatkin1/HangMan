60680bc371ed949f080143133a5fcd33c77c6a7b
Thomas <10twatkin@brightoncollege.net>
2023-09-26 20:42:38 +0100

    Moved 'words.txt' to HangMan\obj\Debug as that's where the hangman.exe file wants to build to.
    Wrote base structure of game to Program.cs, including spaces for all game functions.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,9 +1,87 @@
-﻿namespace HangMan
+﻿using System;
+using System.IO;
+
+namespace HangMan
+
 {
-    internal class Program
+    class Program
     {
+        public static string WordsFilePath = "words.txt"; // Path for words file
+        public static string[] words; // Array to store words from file
+        public static string word; // The word to be guessed
+        public static char[] status; // Current state of guessed word
+        public static int remainingAttempts = 6; // Number of remaining attempts
+        public static bool gameWon = false; // Tracks if game has been won
+        
         public static void Main(string[] args)
         {
+            LoadWords();
+            SelectWord();
+            InitialiseWordStatus();
+            
+            Console.WriteLine("Welcome to Hangman.");
+
+            while (remainingAttempts > 0 && !gameWon) // Main game - calls game functions
+            {
+                DisplayGallows();
+                DisplayStatus();
+                CheckGuess();
+  
+            }
+            
+            // Displays Final result
+            
+            Console.Clear();
+            DisplayGallows();
+            if (gameWon)
+            {
+                Console.WriteLine("You won! Congratulations.");
+            }
+            else
+            {
+                Console.WriteLine("You ran out of attempts. The word was '"+word+"'.");
+            }
+            
+        }
+
+        public static void LoadWords() // Loads words from file
+        {
+            words = File.ReadAllLines(WordsFilePath);
+        }
+
+        
+        public static void SelectWord() // Selects random word
+        {
+            // (Word selector function goes here)
+        }
+
+        static void InitialiseWordStatus() // Makes the status of the word, i.e. '___' 
+        {
+            // (Initialise word status function goes here)
+        }
+
+        static void DisplayGallows() // Displays gallows
+        {
+            // (Gallows function goes here)
+        }
+
+        static void UpdateGuessedWord(char guess) // Updates word status
+        {
+            // (Word status update function goes here)
+        }
+
+        static void DisplayStatus() // Displays word status
+        {
+            // (Word status display function goes here)
+        }
+
+        static void GetUserGuess() // Gets guess from user
+        {
+            // (Get user guess function goes here)
+        }
+        static void CheckGuess() // Checks guess
+        {
+            // (Guess check function goes here)
         }
     }
 }
